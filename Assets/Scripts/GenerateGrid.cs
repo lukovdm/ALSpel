99414cfbb540c2aabf0d9b318036944329c487ca
Luko van der Maas <lukovandermasa@gmail.com>
2015-08-15 19:25:47 +0200

    Added a halve done grid.

---
+++ Assets/Scripts/GenerateGrid.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateGrid : MonoBehaviour {
+
+	[SerializeField] GameObject square; 
+	[SerializeField] int gridX = 10;
+	[SerializeField] int gridY = 10;
+
+	void Start() {
+		for (int x = 0; x < gridX; x++) {
+			for (int y = 0; y < gridY; y++){
+				GameObject instSquare = (GameObject) Instantiate(square, new Vector3((float) (-(gridX * 0.5 * square.transform.localScale.x) + x * square.transform.localScale.x), 
+				                                                        (float) (-(gridY * 0.5 * square.transform.localScale.y) + y * square.transform.localScale.y)), Quaternion.identity);
+				instSquare.transform.SetParent(transform, true);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/ResizeGrid.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResizeGrid : MonoBehaviour {
+
+	[SerializeField] Vector3 gridPos;
+	Camera camera = new Camera();
+
+	void Start () {
+		camera = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera> ();
+	}
+
+	void Update () {
+		Vector3 scale = new Vector3 (camera.ScreenToWorldPoint (gridPos).x / transform.GetChild (0).localPosition.x, 
+		                             camera.ScreenToWorldPoint (gridPos).x / transform.GetChild (0).localPosition.x);
+		//Vector3 pos = new Vector3 (0, camera.ScreenToWorldPoint(gridPos)
+		transform.localScale = scale;
+	}
+}
